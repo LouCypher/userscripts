9e9438b76bd299ca46ea1fcad1fcd8343b0e7501
LouCypher <me@loucypher.mp>
2012-07-07 19:17:29 +0700

    getGlobalValue.js
    https://gist.github.com/937d82d7a9c5d8d3ffcf

---
+++ getGlobalValue.js/README.md
@@ -0,0 +1,23 @@
+# getGlobalValue.js
+
+[User script](http://wiki.greasespot.net/User_script) function to get values of global variables in a web page without using [unsafeWindow](http://wiki.greasespot.net/unsafeWindow).
+
+## Compatibility
+### Browsers
+* Chrome 10+
+* Firefox 4+ (with [Greasemonkey](https://addons.mozilla.org/addon/greasemonkey/) or [Scriptish](https://addons.mozilla.org/addon/scriptish))
+* Opera 10+
+* Safari 5+ (with [NinjaKit](https://github.com/os0x/NinjaKit))
+* SeaMonkey 2.1+ (with Greasemonkey or Scriptish)
+
+### Extensions
+* Greasemonkey 0.9+
+* Scriptish 0.1.7+
+* NinjaKit 0.9.1+
+
+## How to use this script
+Copy [script content](https://gist.github.com/raw/937d82d7a9c5d8d3ffcf/getGlobalValue.js) (including the license) into your user script or use [@require](http://wiki.greasespot.net/Metadata_Block#.40require) metadata for Greasemonkey/Scriptish/NinjaKit
+
+```javascript
+// @require  https://gist.github.com/raw/937d82d7a9c5d8d3ffcf/getGlobalValue.js
+```
---
+++ getGlobalValue.js/examples/mediawiki.user.js
@@ -0,0 +1,87 @@
+// ==UserScript==
+// @name            MediaWiki Get Global Value Example
+// @namespace       http://www.wikia.com/User:LouCypher
+// @description     Get variables from content window
+// @include         http://*.wikia.com/*
+// @include         http://*.wikipedia.org/*
+// @include         https://*.wikipedia.org/*
+// @include         http://wiki.greasespot.net/*
+// ==/UserScript==
+
+(function() { // function wrapper for Opera
+
+  if (frameElement) return; // Don't run inside frame/iframe
+
+  var sitename = getGlobalValue("wgSiteName");
+  var username = getGlobalValue("wgUserName");
+  var usergroups = getGlobalValue("wgUserGroups");
+  var action = getGlobalValue("wgAction");  // viewing/editing/submitting
+  var title = getGlobalValue("wgTitle");
+  var msg = "Welcome to " + sitename
+          + (username ? (", " + username) : "") + ".\n"
+          + "You are " + action + "ing \"" + title + "\" article.\n"
+          + "You're " + (usergroups ? ("a " + usergroups) : "not logged in")
+          + ".";
+
+  console.log(msg);
+
+  //@https://gist.github.com/937d82d7a9c5d8d3ffcf
+  /*
+      Get value a global variable from user script. Version 1.3
+      Copyright (C) 2012 LouCypher
+
+      This program is free software: you can redistribute it and/or modify
+      it under the terms of the GNU General Public License as published by
+      the Free Software Foundation, either version 3 of the License, or
+      (at your option) any later version.
+
+      This program is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+      GNU General Public License for more details.
+
+      You should have received a copy of the GNU General Public License
+      along with this program. If not, see <http://www.gnu.org/licenses/>
+  */
+
+  /**
+   * Get the value of a global variable.
+   *
+   * @param aGlobalVarName
+   *        String. The name of global variable.
+   *        To get property of a global object, use "object['property']"
+   *        or "object['property1']['property2']".
+   * @param debug [optional]
+   *        Boolean. If true, display 'variable = value' in console.
+   * @returns The value of aGlobalVarName.
+   *          If aGlobalVarName is undefined, returns null.
+   */
+  function getGlobalValue(aGlobalVarName, debug) {
+    var script = document.querySelector("head")
+                         .appendChild(document.createElement("script"));
+    script.type = "text/javascript";
+    
+    // Unique name for sessionStorage
+    var itemName = "globalValue_" + (new Date()).getTime().toString();
+
+    // Store global value to sessionStorage
+    script.textContent = "sessionStorage['" + itemName + "'] = " +
+                         "JSON.stringify({'value' : " + aGlobalVarName + "})";
+
+    var globalValue;
+    try {
+      // Get global value from sessionStorage
+      globalValue = JSON.parse(sessionStorage[itemName]).value;
+    } catch (ex) {}
+
+    // Clean up
+    script.parentNode.removeChild(script); // Remove <script> from DOM
+    sessionStorage.removeItem(itemName); // Remove sessionStorage item
+
+    debug && console.log(aGlobalVarName + " = " + globalValue);
+
+    return globalValue; // Returns the value of aGlobalVarName
+                        // Returns null if aGlobalVarName is undefined
+  }
+
+})()
