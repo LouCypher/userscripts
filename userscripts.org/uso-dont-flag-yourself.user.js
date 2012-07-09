39e2d4d10e8b47dd227794ffe0763d5d9260339e
LouCypher <me@loucypher.mp>
2012-07-09 15:21:16 +0700

    Remove 'Flag Spam' button from your own posts

---
+++ userscripts.org/uso-dont-flag-yourself.user.js
@@ -0,0 +1,35 @@
+/* This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details. */
+
+// ==UserScript==
+// @name          USO: Don't flag yourself
+// @namespace     http://userscripts.org/users/12
+// @description   Remove 'Flag Spam' button from your own posts
+// @version       1.0
+// @author        LouCypher
+// @license       WTFPL http://sam.zoy.org/wtfpl/COPYING
+// @include       https://userscripts.org/topics/*
+// @include       http://userscripts.org/topics/*
+// @include       http://userscripts.org./topics/*
+// @include       http://greasefire.userscripts.org/topics/*
+// @include       http://greasefire.userscripts.org./topics/*
+// ==/UserScript==
+
+(function() {
+  var loggedin = document.querySelector(".login_status a[href^='/home']");
+  if (!loggedin) return;  // Not logged in.
+
+  var users = document.querySelectorAll(".author.vcard a[user_id]");
+  if (!users.length) return;
+
+  var form;
+  for (var i = 0; i < users.length; i++) {
+    if (users[i].textContent == loggedin.textContent) {
+      form = users[i].parentNode.parentNode.querySelector(".spam_form");
+      form.parentNode.removeChild(form);
+    }
+  }
+})()
