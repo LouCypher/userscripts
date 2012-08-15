51a1eb825b4ca395de8665fe10fdf29e25ad840c
LouCypher <me@loucypher.mp>
2012-08-16 04:02:15 +0700

    adf.ly Redir New userscript

---
+++ adf.ly-redir/adf.ly_Redir.user.js
@@ -0,0 +1,34 @@
+/* This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details. */
+
+// ==UserScript==
+// @name            adf.ly Redir
+// @namespace       http://userscripts.org/users/12
+// @description     Redirect adf.ly to its target location.
+// @version         1.0
+// @author          LouCypher
+// @license         WTFPL http://sam.zoy.org/wtfpl/
+// @resource        license https://github.com/LouCypher/userscripts/raw/master/adf.ly-redir/LICENSE.txt
+// @include         http://adf.ly/*
+// @exclude         http://adf.ly/go/*
+// @grant           none
+// ==/UserScript==
+
+(function() {
+  document.title = "Redirecting\u2026";
+  document.body.textContent = document.title;
+  if (/blocked/.test(location.pathname)) {
+    redir(sessionStorage.getItem("adfly_redirURL"));
+    return;
+  }
+  var script = document.querySelector("script:not([src])");
+  var url = script.textContent.match(/\/go\/.*(?=')/).toString();
+  sessionStorage.setItem("adfly_redirURL", url);
+  redir(url);
+  function redir(aURL) {
+    location.replace(location.href.replace(location.pathname, aURL));
+  }
+})()
