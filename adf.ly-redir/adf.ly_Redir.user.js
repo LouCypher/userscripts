7998ac561efb42c1c06f5ccbaaa4092cae8c9d0c
LouCypher <me@loucypher.mp>
2012-08-17 12:26:07 +0700

    Changed license to GPL

--- adf.ly-redir/adf.ly_Redir.user.js
+++ adf.ly-redir/adf.ly_Redir.user.js
@@ -1,16 +1,28 @@
-/* This program is free software. It comes without any warranty, to
- * the extent permitted by applicable law. You can redistribute it
- * and/or modify it under the terms of the Do What The Fuck You Want
- * To Public License, Version 2, as published by Sam Hocevar. See
- * http://sam.zoy.org/wtfpl/COPYING for more details. */
+/*
+    Redirect adf.ly to its target location.
+    Copyright (C) 2012 LouCypher
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
 
 // ==UserScript==
 // @name            adf.ly Redir
 // @namespace       http://userscripts.org/users/12
 // @description     Redirect adf.ly to its target location.
-// @version         2.0
+// @version         3.0
 // @author          LouCypher
-// @license         WTFPL http://sam.zoy.org/wtfpl/
+// @license         GPL
 // @updateURL       https://userscripts.org/scripts/source/141047.meta.js
 // @resource        license https://github.com/LouCypher/userscripts/raw/master/adf.ly-redir/LICENSE.txt
 // @include         http://adf.ly/*
