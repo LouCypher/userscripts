eaf8139d3f5ee0363786712873b0d108d1d60533
LouCypher <zoolcar9+github@gmail.com>
2012-11-28 22:58:53 +0700

    `image-background.css` for `image-background.user.js`

--- others/image-background.user.js
+++ others/image-background.user.js
@@ -1,27 +1,25 @@
+/* This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details. */
+
 // ==UserScript==
-// @name          Image Background
-// @namespace     http://userscripts.org/users/12
-// @description   Change image background
-// @version       2.0
-// @author        LouCypher
-// @run-at        document-start
-// @include       *
-// @grant         GM_addStyle
+// @name            Single Image Background and Transparency
+// @namespace       http://userscripts.org/users/12
+// @description     Change single image background and show transparency
+// @version         2.0
+// @author          LouCypher
+// @license         WTFPL http://sam.zoy.org/wtfpl/
+// @homepageURL     https://github.com/LouCypher/userscripts
+// @updateURL       https://raw.github.com/LouCypher/userscripts/master/others/image-background.user.js
+// @resource        css https://raw.github.com/LouCypher/userscripts/master/others/image-background.css
+// @resource        license https://raw.github.com/LouCypher/userscripts/master/licenses/WTFPL/LICENSE.txt
+// @run-at          document-start
+// @include         *
+// @grant           GM_addStyle
+// @grant           GM_getResourceText
 // ==/UserScript==
 
-if ("loading" != document.readyState) {
-  throw new Error("This userscript only works with "
-                + "Greasemonkey 0.9.8 or newer.");
-}
-
 if (!/^image\//.test(document.contentType)) return;
-
-GM_addStyle('html, body {\n\
-  background: transparent url("data:image/gif;base64,\\\n\
-R0lGODlhEAAQAIAAAPDz+P///ywAAAAAEAAQAAACH4RvoauIzNyBSyYaLMDZcv15HAaS\\\n\
-IlWiJ5Sya/RWVgEAOw==") 0 0 repeat !important;\n\
-}\n\n\
-img {\n\
-  background: transparent none !important;\n\
-  box-shadow: 0px 0px 9px rgba(0, 0, 0, .5) !important;\n\
-}')
+GM_addStyle(GM_getResourceText("css"));
