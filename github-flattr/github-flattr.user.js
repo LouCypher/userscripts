a5ba16ca023b491c6daa382b9f1d70454e76caed
LouCypher <zoolcar9+github@gmail.com>
2012-12-13 07:07:53 +0700

    Updated with new Gists layout; Cleaned up

--- github-flattr/github-flattr.user.js
+++ github-flattr/github-flattr.user.js
@@ -20,60 +20,23 @@
 // @name          GitHub: Add Flattr button
 // @namespace     https://github.com/LouCypher
 // @description   Add Flattr button on GitHub.com
-// @version       12.2
+// @version       13.0
 // @author        LouCypher
 // @license       GPL
 // @icon          http://i.imgur.com/VDx96.png
-// @resource      license https://github.com/LouCypher/userscripts/raw/master/github-flattr/LICENSE.txt
-// @updateURL     https://userscripts.org/scripts/source/137434.meta.js
-// @downloadURL   https://userscripts.org/scripts/source/137434.user.js
+// @resource      license https://raw.github.com/LouCypher/userscripts/master/licenses/GPL/LICENSE.txt
+// @resource      changelog https://github.com/LouCypher/userscripts/raw/master/github-flattr/changelog.txt
+// @downloadURL   https://github.com/LouCypher/userscripts/raw/master/github-flattr/github-flattr.user.js
 // @include       https://github.com/*
 // @include       https://gist.github.com/*
 // @exclude       https://github.com/dashboard/*
 // @exclude       https://github.com/*/following
-// @grant         none
 // ==/UserScript==
-
-/*
-    Changelog:
-      - 2012-09-24
-          v12: Updated button on user's profile page.
-      - 2012-07-27
-          v11.0: Updated to new repo layout.
-      - 2012-07-26
-          v10.0: Fixed: incorrect commit URL.
-      - 2012-07-19
-          v9.1: Another CSS fix.
-      - 2012-07-18
-          v9.0:
-            - Fixed Flattr button style on gist.
-            - Refactored.
-      - 2012-07-17
-          v8.0: Includes gist.github.com (public gist only).
-      - 2012-07-16
-          v7.0:
-            - Don't add Flattr button on specific pages.
-            - Fixed 'committer is null' error.
-      - 2012-07-15
-          v6.0: Don't add Flattr button on user's dashboard.
-          v5.0: Don't add Flattr button on repo admin page.
-      - 2012-07-07
-          v4.0: Don't add Flattr button on your own commit.
-          v4.0b:
-            - Added Flattr button on commit page.
-            - Reduced Flattr icon size.
-          v3.0: Don't add Flattr button on organizations.
-      - 2012-07-02
-          v2.0: Fixed something.
-      - 2012-07-01
-          v1.0: Initial release.
-*/
 
 (function() {
   var isGist = location.hostname == "gist.github.com";
-  var buttons = isGist ? $("#repos .repo.public .path") // public gist
-                       : $(".site ul.pagehead-actions") ||
-                         $(".user-following-container");
+  var buttons = $(".pagehead ul.pagehead-actions") || // repo/gist page
+                $(".user-following-container");       // user profile page
   if (!buttons ||
 
       // Thou shalt not Flattreth thyself.
@@ -90,25 +53,20 @@
   var url = location.href;
 
   if (isGist) {
-    var owner = $("#owner .name a"), user = $(".userbox .name");
-    if (user && (user.href == owner.href)) return; // Thou shalt not Flattreth thine own snippet.
+    var owner = $(".author.vcard a");
+    var user = $("#user-links .name");
+    if (user && (user.href == owner.href)) return; // Thou shalt not Flattreth thine own gist.
 
-    var button = insertBefore(flattrButton(url.match(/^https:\/\/gist.github.com\/\w+/)),
-                              buttons.lastChild, buttons);
-    button.setAttribute("style",
-                        "color: rgba(0, 0, 0, .8); width: 80px; "
-                      + "height: 18px; font-size: 13px; font-weight: bold; "
-                      + "margin-left: .75em; vertical-align: 1px; "
-                      + "padding: 0 1em 0 2em; border: 1px solid #8b8b8b; "
-                      + "-moz-border-radius: 2em; -o-border-radius: 2em; "
-                      + "-webkit-border-radius: 2em; border-radius: 2em; "
-                      + "background-image: url('https://api.flattr.com/button/"
-                      + "flattr-badge-small.png'), " + getCSSPrefix()
-                      + "linear-gradient(top, #fff, #ddd); "
-                      + "background-position: .75em center, 0 0; "
-                      + "background-repeat: no-repeat, repeat-x; "
-                      + "background-size: 10px 10px, 80px 18px; "
-                      + "text-decoration: none;");
+    var li = insertBefore(document.createElement("li"), $("li", buttons), buttons);
+
+    var button = li.appendChild(document.createElement("button"));
+    button.className = "minibutton";
+    button.appendChild(flattrButton($("a.js-current-repository").href));
+    button.appendChild(button.firstChild.lastChild);
+    button.addEventListener("click", function(e) {
+      e.target.querySelector("a").click();
+    }, false);
+
     return;
   }
 
@@ -126,7 +84,7 @@
                             buttons.firstChild, buttons);
     span.appendChild(flattrButton(url, "Flattr " + name, "minibutton"));
 
-  } else if (!$(".repohead ul.tabs li:last-child a[href$='admin']")) {
+  } else if (!$(".repohead ul.tabs li:last-child a[href$='settings']")) {
   // Thou shalt not Flattreth thine own repo.
     var li = insertBefore(document.createElement("li"),
                           $("li.text", buttons) ? $("li.text", buttons).
@@ -176,9 +134,7 @@
     link.setAttribute("data-flattr-uid", "flattr");
     link.setAttribute("data-flattr-category", "software");
     link.setAttribute("data-flattr-tags", "software, github, opensource");
-    link.innerHTML = isGist
-                     ? "Flattr"
-                     : '<img src="'
+    link.innerHTML = '<img src="'
                      + 'https://api.flattr.com/button/flattr-badge-small.png"'
                      + ' alt="Flattr!" style="width: 12px; height: 12px;'
                      + ' vertical-align: -1px; margin-right: .5em; "/>Flattr!';
