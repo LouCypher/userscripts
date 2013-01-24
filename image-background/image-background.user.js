d99c76cbac3f6089323e04a9cc4d4f7423380b51
LouCypher <zoolcar9+github@gmail.com>
2013-01-25 01:23:37 +0700

    Removed function wrapper

--- image-background/image-background.user.js
+++ image-background/image-background.user.js
@@ -45,239 +45,237 @@
 // @grant           GM_openInTab
 // ==/UserScript==
 
-(function() {
-  var firstTime = GM_getValue("firstTime", true); // Check if first time user
-  if (firstTime) { // If first time user
-    var thanks = GM_getResourceURL("thanks");
-    if (/^data\:/.test(thanks)) { // If old GM_resourceURL
-      thanks = thanks.replace(/text\/plain/, "text/html");
-    }
-    GM_openInTab(thanks); // Open 'thank you' page
-    GM_setValue("firstTime", false); // Don't open 'thank you' page again
+var firstTime = GM_getValue("firstTime", true); // Check if first time user
+if (firstTime) { // If first time user
+  var thanks = GM_getResourceURL("thanks");
+  if (/^data\:/.test(thanks)) { // If old GM_resourceURL
+    thanks = thanks.replace(/text\/plain/, "text/html");
   }
+  GM_openInTab(thanks); // Open 'thank you' page
+  GM_setValue("firstTime", false); // Don't open 'thank you' page again
+}
 
-  if (!/^image\//.test(document.contentType)) {
-    jscolor.binding = false;
-    return;
+if (!/^image\//.test(document.contentType)) {
+  jscolor.binding = false;
+  return;
+}
+
+var html = document.documentElement;
+
+/***** Start checking preferences *****/
+var bgColor = GM_getValue("bgColor", ""); // Get color value pref (def = empty)
+var bgImage = GM_getValue("bgImage", true); // Get background pref (def = true)
+var imgTrans = GM_getValue("imgTrans", true); // Get transparency pref (def = true)
+/***** End checking preferences *****/
+
+setBgColor(bgColor); // Set background color from pref
+setBgImage(bgImage); // Set background patters from pref
+showTransparency(imgTrans); // Set image transparency from pref
+GM_addStyle(GM_getResourceText("css")); // Inject style from @resource
+
+if (!("contextMenu" in html && "HTMLMenuItemElement" in window)) return;
+
+// Append elements
+var div = document.body.appendChild(document.createElement("div"));
+div.innerHTML = GM_getResourceText("htmlElements");
+
+// Check if JavaScript is enabled for JSColor to work
+if (getComputedStyle($("noscript"), null).display == "none") { // If JavaScript is enabled
+  jscolor.dir = "https://raw.github.com/LouCypher/userscripts/master/image-background/jscolor/";
+  $("color-picker").value = bgColor;
+} else { // JavaScript is disabled
+  jscolor.binding = false; // Disable color picker
+  $("error").className = "";
+  var p = $("color-config").querySelector("div");
+  p.replaceChild(document.createTextNode("Enter valid "), p.firstChild);
+}
+
+/***** Start context menu initialization *****/
+// Check/uncheck menu items based on prefs
+bgImage && $("toggle-background-image").setAttribute("checked", "true");
+imgTrans && $("toggle-image-transparency").setAttribute("checked", "true");
+
+// Add event listeners to menuitems
+$("change-background-color").addEventListener("click", showColorConfig, false);
+$("toggle-image-transparency").addEventListener("click", toggleTransparency, false);
+$("toggle-background-image").addEventListener("click", toggleBgImage, false);
+$("help").addEventListener("click", goHelp, false);
+
+// Set context menu to html element
+html.setAttribute("contextmenu", "context-menu");
+html.addEventListener("contextmenu", popupShowing, false);
+/***** End context menu initialization *****/
+
+// Color dialog initialization
+html.addEventListener("click", hidePicker, false);
+$("color-picker").addEventListener("mouseenter", showPicker, false);
+$("color-picker").addEventListener("input", previewBgColor, false);
+$("color-picker").addEventListener("change", previewBgColor, false);
+$("color-config").addEventListener("keypress", function(e) {
+  if (e.altKey || e.ctrlKey || e.metaKey || e.shiftKey) return;
+  switch (e.keyCode) {
+    case 13: // 'Enter' key is pressed
+      saveBgColor();
+      break;
+    case 27: // 'Escape' key is pressed
+      $("color-picker").value = GM_getValue("bgColor", "");
+      resetBgColor();
+    default: return;
   }
+}, false);
+$("ok").addEventListener("click", saveBgColor, false);
+$("cancel").addEventListener("click", resetBgColor, false);
+$("default").addEventListener("click", defaultBgColor, false);
+$("error").addEventListener("click", showAlert, false);
 
-  var html = document.documentElement;
-
-  /***** Start checking preferences *****/
-  var bgColor = GM_getValue("bgColor", ""); // Get color value pref (def = empty)
-  var bgImage = GM_getValue("bgImage", true); // Get background pref (def = true)
-  var imgTrans = GM_getValue("imgTrans", true); // Get transparency pref (def = true)
-  /***** End checking preferences *****/
-
-  setBgColor(bgColor); // Set background color from pref
-  setBgImage(bgImage); // Set background patters from pref
-  showTransparency(imgTrans); // Set image transparency from pref
-  GM_addStyle(GM_getResourceText("css")); // Inject style from @resource
-
-  if (!("contextMenu" in html && "HTMLMenuItemElement" in window)) return;
-
-  // Append elements
-  var div = document.body.appendChild(document.createElement("div"));
-  div.innerHTML = GM_getResourceText("htmlElements");
-
-  // Check if JavaScript is enabled for JSColor to work
-  if (getComputedStyle($("noscript"), null).display == "none") { // If JavaScript is enabled
-    jscolor.dir = "https://raw.github.com/LouCypher/userscripts/master/image-background/jscolor/";
-    $("color-picker").value = bgColor;
-  } else { // JavaScript is disabled
-    jscolor.binding = false; // Disable color picker
-    $("error").className = "";
-    var p = $("color-config").querySelector("div");
-    p.replaceChild(document.createTextNode("Enter valid "), p.firstChild);
+// Executed on right click
+function popupShowing(aEvent) {
+  var node = aEvent.target;
+  while (node && node.id != "color-config") node = node.parentNode;
+  if (node) { // Color config dialog
+    html.removeAttribute("contextmenu"); // Hide context menu items
+  } else {
+    html.setAttribute("contextmenu", "context-menu"); // Show context menu items
   }
+}
 
-  /***** Start context menu initialization *****/
-  // Check/uncheck menu items based on prefs
-  bgImage && $("toggle-background-image").setAttribute("checked", "true");
-  imgTrans && $("toggle-image-transparency").setAttribute("checked", "true");
+// Check validity of color value
+function validateColor(aColor, aCallback) {
+  // Set dummy element color
+  $("dummy").style.color = (aColor == "") ? "#222" : aColor;
+  var save = false;
 
-  // Add event listeners to menuitems
-  $("change-background-color").addEventListener("click", showColorConfig, false);
-  $("toggle-image-transparency").addEventListener("click", toggleTransparency, false);
-  $("toggle-background-image").addEventListener("click", toggleBgImage, false);
-  $("help").addEventListener("click", goHelp, false);
-
-  // Set context menu to html element
-  html.setAttribute("contextmenu", "context-menu");
-  html.addEventListener("contextmenu", popupShowing, false);
-  /***** End context menu initialization *****/
-
-  // Color dialog initialization
-  html.addEventListener("click", hidePicker, false);
-  $("color-picker").addEventListener("mouseenter", showPicker, false);
-  $("color-picker").addEventListener("input", previewBgColor, false);
-  $("color-picker").addEventListener("change", previewBgColor, false);
-  $("color-config").addEventListener("keypress", function(e) {
-    if (e.altKey || e.ctrlKey || e.metaKey || e.shiftKey) return;
-    switch (e.keyCode) {
-      case 13: // 'Enter' key is pressed
-        saveBgColor();
-        break;
-      case 27: // 'Escape' key is pressed
-        $("color-picker").value = GM_getValue("bgColor", "");
-        resetBgColor();
-      default: return;
-    }
-  }, false);
-  $("ok").addEventListener("click", saveBgColor, false);
-  $("cancel").addEventListener("click", resetBgColor, false);
-  $("default").addEventListener("click", defaultBgColor, false);
-  $("error").addEventListener("click", showAlert, false);
-
-  // Executed on right click
-  function popupShowing(aEvent) {
-    var node = aEvent.target;
-    while (node && node.id != "color-config") node = node.parentNode;
-    if (node) { // Color config dialog
-      html.removeAttribute("contextmenu"); // Hide context menu items
-    } else {
-      html.setAttribute("contextmenu", "context-menu"); // Show context menu items
-    }
+  if (getComputedStyle($("dummy"), null).color == "transparent") {
+  // If dummy element's color is not set because invalid color value
+    alert("Invalid color value: " + aColor);
+  } else {
+    aCallback(aColor); // Run callback function
+    save = true;
   }
+  $("dummy").style.color = ""; // Reset dummy element's color
+  return save;
+}
 
-  // Check validity of color value
-  function validateColor(aColor, aCallback) {
-    // Set dummy element color
-    $("dummy").style.color = (aColor == "") ? "#222" : aColor;
-    var save = false;
-
-    if (getComputedStyle($("dummy"), null).color == "transparent") {
-    // If dummy element's color is not set because invalid color value
-      alert("Invalid color value: " + aColor);
-    } else {
-      aCallback(aColor); // Run callback function
-      save = true;
-    }
-    $("dummy").style.color = ""; // Reset dummy element's color
-    return save;
+// Set background color
+function setBgColor(aColorValue) {
+  if (aColorValue == "") {
+    html.style.backgroundColor = ""; // Use default color from CSS resource
+  } else {
+    setStyleProperty(html, "background-color", aColorValue);
   }
+}
 
-  // Set background color
-  function setBgColor(aColorValue) {
-    if (aColorValue == "") {
-      html.style.backgroundColor = ""; // Use default color from CSS resource
-    } else {
-      setStyleProperty(html, "background-color", aColorValue);
-    }
-  }
-
-  // Save color setting to preferences
-  function saveBgColor() {
-    var color = $("color-picker").value;
-    if (validateColor(color, setBgColor)) { // If color value is valid
-      GM_setValue("bgColor", color); // Save color value to pref
-      hideColorConfig();
-    }
-  }
-
-  // Reset background color to previous setting
-  function resetBgColor() {
-    setStyleProperty(html, "background-color", GM_getValue("bgColor", ""));
+// Save color setting to preferences
+function saveBgColor() {
+  var color = $("color-picker").value;
+  if (validateColor(color, setBgColor)) { // If color value is valid
+    GM_setValue("bgColor", color); // Save color value to pref
     hideColorConfig();
   }
+}
 
-  // Use default background color
-  function defaultBgColor() {
-    $("color-picker").value = "";
-    html.style.backgroundColor = "";
-  }
+// Reset background color to previous setting
+function resetBgColor() {
+  setStyleProperty(html, "background-color", GM_getValue("bgColor", ""));
+  hideColorConfig();
+}
 
-  // Show color configuration dialog
-  function showColorConfig() {
-    $("change-background-color").disabled = true; // Disable menu item
-    $("color-config").className = "";
-    $("color-picker").value = GM_getValue("bgColor", "");
-    $("color-picker").focus();
-  }
+// Use default background color
+function defaultBgColor() {
+  $("color-picker").value = "";
+  html.style.backgroundColor = "";
+}
 
-  // Hide color configuration dialog
-  function hideColorConfig() {
-    ("color" in $("color-picker")) && $("color-picker").color.hidePicker();
-    $("color-config").className = "hide"; // Hide dialog
-    $("change-background-color").disabled = false; // Enable menu item
-  }
+// Show color configuration dialog
+function showColorConfig() {
+  $("change-background-color").disabled = true; // Disable menu item
+  $("color-config").className = "";
+  $("color-picker").value = GM_getValue("bgColor", "");
+  $("color-picker").focus();
+}
 
-  // Preview background color when chosing color in color picker
-  function previewBgColor(aEvent) {
-    setBgColor(aEvent.target.value);
-  }
+// Hide color configuration dialog
+function hideColorConfig() {
+  ("color" in $("color-picker")) && $("color-picker").color.hidePicker();
+  $("color-config").className = "hide"; // Hide dialog
+  $("change-background-color").disabled = false; // Enable menu item
+}
 
-  // Show color picker
-  function showPicker(aEvent) {
-    ("color" in $("color-picker")) && $("color-picker").color.showPicker();
-  }
+// Preview background color when chosing color in color picker
+function previewBgColor(aEvent) {
+  setBgColor(aEvent.target.value);
+}
 
-  // Hide color picker
-  function hidePicker(aEvent) {
-    var node = aEvent.target;
-    if ((node.localName != "div") && !node.id && ("color" in $("color-picker"))) {
-      $("color-picker").color.hidePicker();
-    }
-  }
+// Show color picker
+function showPicker(aEvent) {
+  ("color" in $("color-picker")) && $("color-picker").color.showPicker();
+}
 
-  function showAlert() {
-    var site = (location.protocol == "file:") ? "file://" : location.hostname;
-    alert("Color picker requires JavaScript to be enabled. However,\n" +
-          "you can still change the background color by entering any\n" +
-          "valid color value.\n\nIf you have NoScript extension, color " +
-          "picker will work if\nyou click 'Allow " + site +
-          "' from NoScript menu.");
+// Hide color picker
+function hidePicker(aEvent) {
+  var node = aEvent.target;
+  if ((node.localName != "div") && !node.id && ("color" in $("color-picker"))) {
+    $("color-picker").color.hidePicker();
   }
+}
 
-  // Enable/disable background patterns
-  function setBgImage(aBoolean) {
-    switch(aBoolean) {
-      case true: // Enable background patterns
-        html.style.backgroundImage = ""; // Use bg patterns in CSS resource
-        break;
-      case false: // Disable background patterns
-        setStyleProperty(html, "background-image", "none");
-    }
-    GM_setValue("bgImage", aBoolean); // Save background option to pref
-  }
+function showAlert() {
+  var site = (location.protocol == "file:") ? "file://" : location.hostname;
+  alert("Color picker requires JavaScript to be enabled. However,\n" +
+        "you can still change the background color by entering any\n" +
+        "valid color value.\n\nIf you have NoScript extension, color " +
+        "picker will work if\nyou click 'Allow " + site +
+        "' from NoScript menu.");
+}
 
-  // Toggle background patterns on/off
-  function toggleBgImage(aEvent) {
-    setBgImage(aEvent.target.checked);
+// Enable/disable background patterns
+function setBgImage(aBoolean) {
+  switch(aBoolean) {
+    case true: // Enable background patterns
+      html.style.backgroundImage = ""; // Use bg patterns in CSS resource
+      break;
+    case false: // Disable background patterns
+      setStyleProperty(html, "background-image", "none");
   }
+  GM_setValue("bgImage", aBoolean); // Save background option to pref
+}
 
-  // Enable/disable image transparency
-  function showTransparency(aBoolean) {
-    var styleId = "transparent-image";
-    var style = null;
-    switch (aBoolean) {
-      case true: // Enable image transparency
-        // Inject style
-        style = document.head.appendChild(document.createElement("style"));
-        style.textContent = "img { background-color: transparent !important; }";
-        style.id = styleId;
-        break;
-      case false: // Disable image transparency
-        style = $(styleId);
-        style && style.parentNode.removeChild(style); // Remove style if exists
-    }
-    GM_setValue("imgTrans", aBoolean); // Save image transparency option to pref
-  }
+// Toggle background patterns on/off
+function toggleBgImage(aEvent) {
+  setBgImage(aEvent.target.checked);
+}
 
-  // Toggle image transparency on/off
-  function toggleTransparency(aEvent) {
-    showTransparency(aEvent.target.checked);
+// Enable/disable image transparency
+function showTransparency(aBoolean) {
+  var styleId = "transparent-image";
+  var style = null;
+  switch (aBoolean) {
+    case true: // Enable image transparency
+      // Inject style
+      style = document.head.appendChild(document.createElement("style"));
+      style.textContent = "img { background-color: transparent !important; }";
+      style.id = styleId;
+      break;
+    case false: // Disable image transparency
+      style = $(styleId);
+      style && style.parentNode.removeChild(style); // Remove style if exists
   }
+  GM_setValue("imgTrans", aBoolean); // Save image transparency option to pref
+}
 
-  function goHelp() {
-    GM_openInTab("https://github.com/LouCypher/userscripts/blob/master/image-background/readme.md#change-background-color");
-  }
+// Toggle image transparency on/off
+function toggleTransparency(aEvent) {
+  showTransparency(aEvent.target.checked);
+}
 
-  function setStyleProperty(aNode, aPropertyName, aValue) {
-    aNode.style.setProperty(aPropertyName, aValue, "important");
-  }
+function goHelp() {
+  GM_openInTab("https://github.com/LouCypher/userscripts/blob/master/image-background/readme.md#change-background-color");
+}
 
-  function $(aId) {
-    return document.getElementById(aId);
-  }
-})()
+function setStyleProperty(aNode, aPropertyName, aValue) {
+  aNode.style.setProperty(aPropertyName, aValue, "important");
+}
+
+function $(aId) {
+  return document.getElementById(aId);
+}
