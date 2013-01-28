cb87443e46fd8ebd67b9315ea0433cdff5d3d1b2
LouCypher <zoolcar9+github@gmail.com>
2013-01-29 04:20:34 +0700

    Added linkbucks-redir.user.js

---
+++ linkbucks-redir/linkbucks-redir.user.js
@@ -0,0 +1,43 @@
+/*
+    Redirect LinkBucks to its target location.
+    Copyright (C) 2012 LouCypher
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
+
+// ==UserScript==
+// @name            LinkBucks Redir
+// @namespace       http://userscripts.org/users/12
+// @description     Redirect LinkBucks to its target location.
+// @version         1.0
+// @author          LouCypher
+// @license         GPL
+// @resource        license https://raw.github.com/LouCypher/userscripts/master/licenses/GPL/LICENSE.txt
+// @include         http://*.linkbucks.com/
+// ==/UserScript==
+
+(function() {
+  var scripts = document.querySelectorAll("body > script:not([src])");
+  if (!scripts.length) return;
+  document.body.innerHTML = document.title = "Redirecting\u2026";
+  var regx = /TargetUrl.*\'http.*(?=')/
+  for (var i = 0; i < scripts.length; i++) {
+    if (regx.test(scripts[i].textContent)) {
+      var targetURL = scripts[i].textContent.match(regx).toString()
+                                .replace(/TargetUrl.*\'/, "");
+      location.replace(targetURL);
+      return;
+    }
+  }
+})()
