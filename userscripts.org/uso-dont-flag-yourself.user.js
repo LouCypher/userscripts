679860cb220e77ef372e09c81945d073d22eff71
LouCypher <zoolcar9+github@gmail.com>
2013-02-11 05:37:51 +0700

    Updated uso-dont-flag-yourself.user.js

--- userscripts.org/uso-dont-flag-yourself.user.js
+++ userscripts.org/uso-dont-flag-yourself.user.js
@@ -8,30 +8,49 @@
 // @name            USO: Don't flag yourself
 // @namespace       http://userscripts.org/users/12
 // @description     Remove 'Flag Spam' button from your own posts
-// @version         1.1
+// @version         2.0
 // @author          LouCypher
 // @license         WTFPL http://www.wtfpl.net/
 // @homepageURL     https://userscripts.org/scripts/show/138063
 // @downloadURL     https://raw.github.com/LouCypher/userscripts/master/userscripts.org/uso-dont-flag-yourself.user.js
 // @updateURL       https://raw.github.com/LouCypher/userscripts/master/userscripts.org/uso-dont-flag-yourself.user.js
-// @resource        license https://raw.github.com/LouCypher/userscripts/master/licenses/WTFPL/LICENSE.txt
+// @resource        LICENSE https://raw.github.com/LouCypher/userscripts/master/licenses/WTFPL/LICENSE.txt
+// @include         https://userscripts.org/users/*/posts*
 // @include         https://userscripts.org/topics/*
+// @include         https://userscripts.org/posts*
+// @include         http://userscripts.org/users/*/posts*
 // @include         http://userscripts.org/topics/*
+// @include         http://userscripts.org/posts*
+// @include         http://userscripts.org./users/*/posts*
 // @include         http://userscripts.org./topics/*
+// @include         http://userscripts.org./posts*
+// @include         http://greasefire.userscripts.org/users/*/posts*
 // @include         http://greasefire.userscripts.org/topics/*
+// @include         http://greasefire.userscripts.org/posts*
 // @include         http://greasefire.userscripts.org./topics/*
+// @include         http://greasefire.userscripts.org./posts*
+// @grant           none
 // ==/UserScript==
 
 (function() {
   var loggedin = document.querySelector(".login_status a[href^='/home']");
   if (!loggedin) return;  // Not logged in.
 
-  var users = document.querySelectorAll(".author.vcard a[user_id]");
-  if (!users.length) return;
+  var user = document.querySelector("#section .container h2 a");
+  if (user && (user.textContent === loggedin.textContent)) {
+    var forms = document.querySelectorAll("#main .posts.wide .post.hentry form[action='/spam']");
+    if (!forms.length) return;
+    for (var i = 0; i < forms.length; i++) {
+      forms[i].parentNode.removeChild(forms[i]);
+    }
+    return;
+  }
 
+  var users = document.querySelectorAll(".author.vcard a[href^='/users/']");
+  if (!users.length) return;
   var form;
   for (var i = 0; i < users.length; i++) {
-    if (users[i].textContent == loggedin.textContent) {
+    if (users[i].textContent === loggedin.textContent) {
       form = users[i].parentNode.parentNode.querySelector("form[action='/spam']");
       form.parentNode.removeChild(form);
     }
