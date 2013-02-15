30482b01f3a8cfd275274bd5930a7da3cf3fe3d2
LouCypher <zoolcar9+github@gmail.com>
2013-02-15 10:52:18 +0700

    Updated tumblr-tumview-linker.user.js Changed license to GPL

--- others/tumblr-tumview-linker.user.js
+++ others/tumblr-tumview-linker.user.js
@@ -1,20 +1,32 @@
-/* This program is free software. It comes without any warranty, to
- * the extent permitted by applicable law. You can redistribute it
- * and/or modify it under the terms of the Do What The Fuck You Want
- * To Public License, Version 2, as published by Sam Hocevar. See
- * http://www.wtfpl.net/ for more details. */
+/*
+    Add Tumview.com link on Tumblr sites and vice versa.
+    Copyright (C) 2012 LouCypher
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>
+*/
 
 // ==UserScript==
 // @name            Tumblr Tumview Linker
 // @namespace       http://userscripts.org/users/12
-// @description     Add Tumview.com link on Tumblr sites. Works on custom domains.
-// @version         3.0
+// @description     Add Tumview.com link on Tumblr sites and vice versa.
+// @version         3.0.1
 // @author          LouCypher
-// @license         WTFPL http://www.wtfpl.net/
+// @license         GPL
 // @homepageURL     https://userscripts.org/scripts/show/158464
 // @downloadURL     https://raw.github.com/LouCypher/userscripts/master/others/tumblr-tumview-linker.user.js
 // @updateURL       https://raw.github.com/LouCypher/userscripts/master/others/tumblr-tumview-linker.user.js
-// @resource        LICENSE https://raw.github.com/LouCypher/userscripts/master/licenses/WTFPL/LICENSE.txt
+// @resource        LICENSE https://raw.github.com/LouCypher/userscripts/master/licenses/GPL/LICENSE.txt
 // !resource        CHANGELOG https://raw.github.com/LouCypher/userscripts/master/others/tumblr-tumview-linker.txt
 // @include         *
 // @grant           none
