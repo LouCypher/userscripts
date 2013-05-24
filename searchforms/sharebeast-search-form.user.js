5280f45a5e96b863c5e9b265b0d40bedd9d9cbbc
LouCypher <zoolcar9+dev@gmail.com>
2013-05-25 06:28:29 +0700

    Fixed CSS compatibility

--- searchforms/sharebeast-search-form.user.js
+++ searchforms/sharebeast-search-form.user.js
@@ -9,7 +9,7 @@
 // @name            Sharebeast - search form
 // @namespace       http://userscripts.org/users/12
 // @description     Add Google search form on sharebeast.com
-// @version         1.1
+// @version         1.2
 // @author          LouCypher
 // @license         WTFPL http://www.wtfpl.net/
 // @contributionURL http://loucypher.github.io/userscripts/donate.html?Sharebeast+-+search+form
@@ -51,18 +51,24 @@ function addSearchForm() {
         node.textContent = aCSS;
       }
     }
-    GM_addStyle('#menu form{float:right}' +
-                '#menu form input{padding:0 .5em}' +
-                '#menu form input[name="q"]{' +
-                'height:30px;border:1px solid #ccc}' +
-                '#menu form input[type="submit"]{' +
-                'height:32px;text-align:center;color:#fff;font-weight:bold;' +
-                'text-shadow:0 1px 0 rgba(0, 32, 0, .8);' +
-                'background-image:linear-gradient(to bottom,#57d100 25%,' +
-                '#008700);border:none}' +
-                '#menu form input[type="submit"]:hover{' +
-                'background-image:linear-gradient(to bottom,#57d100 50%,' +
-                '#008700)}');
+    GM_addStyle('\
+      #menu form{float:right}\
+      #menu form input{padding:0 .5em}\
+      #menu form input[name="q"]{height:30px;border:1px solid #ccc}\
+      #menu form input[type="submit"]{\
+      height:32px;text-align:center;color:#fff;font-weight:bold;\
+      text-shadow:0 1px 0 rgba(0, 32, 0, .8);\
+      background-image:-moz-linear-gradient(top,#57d100 25%,#008700);\
+      background-image:-o-linear-gradient(top,#57d100 25%,#008700);\
+      background-image:-webkit-linear-gradient(top,#57d100 25%,#008700);\
+      background-image:linear-gradient(to bottom,#57d100 25%,#008700);\
+      border:none}\
+      #menu form input[type="submit"]:hover{\
+      background-image:-moz-linear-gradient(top,#57d100 50%,#008700)\
+      background-image:-o-linear-gradient(top,#57d100 50%,#008700)\
+      background-image:-webkit-linear-gradient(top,#57d100 50%,#008700)\
+      background-image:linear-gradient(to bottom,#57d100 50%,#008700)\
+    }');
     var form = menu.appendChild(document.createElement("form"));
     form.outerHTML = '<form action="//www.google.com/search" method="get">'
                    + '<input type="hidden" name="sitesearch"'
