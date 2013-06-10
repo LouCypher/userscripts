fc9440b6cd1f38a73a3d912d9a85a22af5ed99b4
LouCypher <zoolcar9+dev@gmail.com>
2013-06-10 11:15:15 +0700

    Use `@resource` and `DOMParser` to insert HTML elements

--- scriptish/about-new-tab/about-new-tab.user.js
+++ scriptish/about-new-tab/about-new-tab.user.js
@@ -12,7 +12,7 @@
 // @name            about:newtab
 // @namespace       http://mozilla.status.net/loucypher
 // @description     Add input fields to change rows and columns setting on about:newtab page.
-// @version         2.1
+// @version         3.0a1
 // @author          LouCypher
 // @contributor     Benjamin Humphrey - icons http://findicons.com/icon/554396/64_thumbnails
 // @license         MPL 2.0
@@ -24,7 +24,8 @@
 // @updateURL       https://raw.github.com/LouCypher/userscripts/master/scriptish/about-new-tab/about-new-tab.user.js
 // @downloadURL     https://raw.github.com/LouCypher/userscripts/master/scriptish/about-new-tab/about-new-tab.user.js
 // @resource        favicon https://raw.github.com/LouCypher/userscripts/master/scriptish/about-new-tab/favicon.ico
-// @resource        CSS https://raw.github.com/LouCypher/userscripts/master/scriptish/about-new-tab/about-new-tab.css
+// @resource        CSS  https://raw.github.com/LouCypher/userscripts/master/scriptish/about-new-tab/about-new-tab.css
+// @resource        HTML https://raw.github.com/LouCypher/userscripts/master/scriptish/about-new-tab/about-new-tab.html
 // @resource        CHANGELOG https://raw.github.com/LouCypher/userscripts/master/scriptish/about-new-tab/CHANGELOG.txt
 // @resource        LICENSE https://raw.github.com/LouCypher/userscripts/master/licenses/MPL/LICENSE.txt
 // @include         about:newtab
@@ -41,52 +42,27 @@
     }'
   let sss = Components.classes["@mozilla.org/content/style-sheet-service;1"]
                       .getService(Ci.nsIStyleSheetService);
-  let uri = Services.io.newURI("data:text/css," +
-                               "/*about:newtab user script*/" +
+  let uri = Services.io.newURI("data:text/css,/*about:newtab userscript*/" +
                                encodeURIComponent(css), null, null);
   if (!sss.sheetRegistered(uri, sss.USER_SHEET))
     sss.loadAndRegisterSheet(uri, sss.USER_SHEET);
 
   /***** Begin initializations *****/
 
-  var NewTab = {
-    get prefs() {
-      return Services.prefs.getBranch("browser.newtabpage.");
-    },
-
-    get columns() {
-      return this.prefs.getIntPref("columns");
-      return gGridPrefs.gridColumns;
-    },
-
-    get rows() {
-      //return this.prefs.getIntPref("rows");
-      return gGridPrefs.gridRows;
-    },
-
-    setPref: function setPref(aRowsOrColumns, aInt) {
-      this.prefs.setIntPref(aRowsOrColumns, aInt);
-    },
-
-    createElement: function createElement(aElement) {
-      return document.createElementNS(HTML_NAMESPACE, aElement);
-    }
-  }
-
-  var style = document.documentElement.appendChild(NewTab.createElement("style"));
+  var style = document.documentElement.appendChild(document.createElementNS(HTML_NAMESPACE, "style"));
   style.type = "text/css";
   style.textContent = GM_getResourceText("CSS");
 
-  var divS = NewTab.createElement("div");
-  divS.id = "newtab-settings";
+  var divS = (new DOMParser).parseFromString(GM_getResourceText("HTML"), "application/xml")
+                            .documentElement;
 
   if (typeof newTabTools === "object") { // If New Tab Tools extension is enabled
-    gAllPages.enabled = true; // Show thumbnails
+    gAllPages.enabled = true; // Always show thumbnails
 
     $("#config-inner").insertBefore(divS, $("#config-morePrefs"));
     $("#config-inner").insertBefore(document.createElement("spacer"), $("#config-morePrefs"));
 
-    let label = divS.appendChild(document.createElement("label"));
+    let label = $("#config-inner").insertBefore(document.createElement("label"), divS);
     label.className = "header";
     label.value = "Rows and Columns:";
 
@@ -97,55 +73,33 @@
     $("#config-title-input") && $("#config-title-input").removeAttribute("flex");
     $("#config-morePrefs").style.color = "inherit";
   }
-
   else
     $("#newtab-horizontal-margin").insertBefore(divS, $(".newtab-side-margin:last-child"));
 
-  var divF = divS.appendChild(NewTab.createElement("div"));
-  divF.id = "newtab-form";
+  ["#setting-columns", "#setting-rows"].forEach(function(aSelector) {
+    $(aSelector).value = getIntPref(aSelector.match(/[a-z]+$/).toString());
+    $(aSelector).addEventListener("change", setValueFromInput);
+    $(aSelector).addEventListener("DOMMouseScroll", mouseWheel); // Change value with mouse scroll
+  })
 
-  divF.appendChild(addInputField("Number of columns", "setting-columns", NewTab.columns));
-  divF.appendChild(addInputField("Number of rows", "setting-rows", NewTab.rows));
-
-  // Reset button to reset thumbnails row and columns settings to default (3x3)
-  var rButton = divF.appendChild(NewTab.createElement("input"));
-  rButton.type = "reset";
-  rButton.value = "Reset to default";
-  rButton.addEventListener("click", function() {
-    NewTab.setPref("columns", $("#setting-columns").value = 3);
+  $('#newtab-form input[type="reset"]').addEventListener("click", function() {
+    setIntPref("columns", $("#setting-columns").value = 3);
     $("#setting-columns").classList.add("default-value");
-    NewTab.setPref("rows", $("#setting-rows").value = 3);
+    setIntPref("rows", $("#setting-rows").value = 3);
     $("#setting-rows").classList.add("default-value");
   })
 
-  // Thumbnails button to toggle thumbnail view
-  var tButton = divF.appendChild(NewTab.createElement("input"));
-  tButton.type = "button";
-  tButton.title = newTabString("show");
-  tButton.addEventListener("click", function() {
+  $('#newtab-form input[type="button"]').addEventListener("click", function() {
     gAllPages.enabled = !gAllPages.enabled;
   })
 
-  function addInputField(aLabel, aId, aValue) {
-    let div = NewTab.createElement("div");
 
-    let label = div.appendChild(NewTab.createElement("label"));
-    label.textContent = aLabel;
-    label.setAttribute("for", aId);
+  function setIntPref(aRowsOrColumns, aInt) {
+    Services.prefs.setIntPref("browser.newtabpage." + aRowsOrColumns, aInt);
+  }
 
-    let input = div.appendChild(NewTab.createElement("input"));
-    input.id = aId;
-    input.type = "text";
-    input.value = aValue;
-    input.size = "4";
-    if (aValue == 3) input.className = "default-value";
-
-    input.addEventListener("change", setValueFromInput);
-  //input.addEventListener("input", setValueFromInput);   // Didn't work well
-  //input.addEventListener("keydown", arrowUpDown);       // Conflicted with change event
-    input.addEventListener("DOMMouseScroll", mouseWheel); // Change value with mouse scroll
-
-    return div;
+  function getIntPref(aRowsOrColumns, aInt) {
+    return Services.prefs.getIntPref("browser.newtabpage." + aRowsOrColumns);
   }
 
   // Save input value to preference
@@ -161,10 +115,11 @@
     else
       input.classList.remove("default-value");
     
-    NewTab.setPref(input.id.match(/[a-z]+$/).toString(), input.value);
+    setIntPref(input.id.match(/[a-z]+$/).toString(), input.value);
   }
 
   // Use mouse scroll to increase/decrease value in text input
+  // https://developer.mozilla.org/DOM/DOM_event_reference/DOMMouseScroll
   function mouseWheel(aEvent) {
     let input = aEvent.target;
     if (aEvent.detail > 0) {                // Scroll down
@@ -175,24 +130,6 @@
       if (input.value < 50) input.value++;  // Increase number if value < 50
       else aEvent.stopPropagation();        // else stop (max = 50)
     }
-    setValueFromInput(aEvent);
-  }
-
-  // Use up/down key to increase/decrease value in text input
-  function arrowUpDown(aEvent) {
-    let input = aEvent.target;
-    switch (aEvent.keyCode) {
-      case 38:  // Up arrow
-        if (input.value < 50) input.value++;  // Increase number if value < 50
-        else aEvent.stopPropagation();        // else stop (max = 50)
-        break;
-      case 40:  // Down arrow
-        if (input.value > 1) input.value--;   // Decrease number if value > 1
-        else aEvent.stopPropagation();        // else stop (min = 1)
-        break;
-      default: aEvent.stopPropagation();
-    }
-    console.log(input.value);
     setValueFromInput(aEvent);
   }
 
