62539141946bb23aa95b6fbc3d7654f432e27c19
LouCypher <zoolcar9+dev@gmail.com>
2013-07-22 01:04:39 +0700

    update

--- others/imageshack-continue-to-the-image-links.user.js
+++ others/imageshack-continue-to-the-image-links.user.js
@@ -2,13 +2,14 @@
 // @name            ImageShack - Continue to the image links
 // @namespace       http://userscripts.org/users/12
 // @description     Continue to the image links after uploading
-// @version         0.0
+// @version         0.1
 // @license         public domain
 // @downloadURL     https://raw.github.com/LouCypher/userscripts/master/others/imageshack-continue-to-the-image-links.user.js
 // @updateURL       https://raw.github.com/LouCypher/userscripts/master/others/imageshack-continue-to-the-image-links.user.js
-// @include         http://imageshack.us/content-round.php?*page=done*
+// @include         /^http://imageshack\.us/content_round\.php\?.*page=done.*/
 // @run-at          document-start
 // @grant           none
 // ==/UserScript==
 
-location.replace(location.href + "&sa=0");
+if (!/\&sa\=0$/.test(location.search))
+  location.replace(location.href + "&sa=0");
