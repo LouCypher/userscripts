3371e7f03eca7eead6e6323b4c4c18efa8540944
LouCypher <zoolcar9+dev@gmail.com>
2013-07-31 11:51:18 +0700

    Added more domains

--- adf.ly-redir/linkbucks-redir.user.js
+++ adf.ly-redir/linkbucks-redir.user.js
@@ -20,7 +20,7 @@
 // @name            LinkBucks Redir
 // @namespace       http://userscripts.org/users/12
 // @description     Redirect LinkBucks to its target location.
-// @version         1.1
+// @version         2.0
 // @author          LouCypher
 // @license         GPL
 // @homepageURL     https://github.com/LouCypher/userscripts/tree/master/adf.ly-redir
@@ -29,19 +29,55 @@
 // @resource        license https://raw.github.com/LouCypher/userscripts/master/licenses/GPL/LICENSE.txt
 // @include         http://*.linkbucks.com/
 // @include         http://*.any.gs/
+// @include         http://*.amy.gs/
+// @include         http://*.deb.gs/
+// @include         http://*.dyo.gs/
+// @include         http://*.theseforums.com/
+// @include         http://*.theseblogs.com/
+// @include         http://*.ubervidz.com/
+// @include         http://*.uberpicz.com/
+// @include         http://*.zxxo.net/
+// @include         http://*.ugalleries.net/
+// @include         http://*.picturesetc.net/
+// @include         http://*.filesonthe.net/
+// @include         http://*.cash4files.com/
+// @include         http://*.cash4links.co/
+// @include         http://*.miniurls.co/
+// @include         http://*.ultrafiles.net/
+// @include         http://*.drstickyfingers.com/
+// @include         http://*.allanalpass.com/
+// @include         http://*.freean.us/
+// @include         http://*.freegaysitepass.com/
+// @include         http://*.fapoff.com/
+// @include         http://*.galleries.bz/
+// @include         http://*.goneviral.com/
+// @include         http://*.hornywood.tv/
+// @include         http://*.sexpalace.gs/
+// @include         http://*.linkbabes.com/
+// @include         http://*.linkseer.net/
+// @include         http://*.megaline.co/
+// @include         http://*.picbucks.com/
+// @include         http://*.poontown.net/
+// @include         http://*.seriousdeals.net/
+// @include         http://*.seriousfiles.com/
+// @include         http://*.urlbeat.net/
+// @include         http://*.youfap.me/
+// @include         http://*.whackyvidz.com/
+// @include         http://*.tubeviral.com/
+// @include         http://*.tnabucks.com/
+// @include         http://*.tinylinks.co/
+// @include         http://*.rqq.co/
+// @include         http://*.qqc.co/
+// @include         http://*.yyv.co/
+// @include         http://*.zff.co/
+// @grant           none
 // ==/UserScript==
 
-(function() {
-  var scripts = document.querySelectorAll("body > script:not([src])");
-  if (!scripts.length) return;
+var xpath = "./script[not(@src) and text()[contains(.,'TargetUrl =')]]";
+var script = document.evaluate(xpath, document.body, null, 9, null).singleNodeValue;
+if (script) {
   document.body.innerHTML = document.title = "Redirecting\u2026";
-  var regx = /TargetUrl.*\'http.*(?=')/
-  for (var i = 0; i < scripts.length; i++) {
-    if (regx.test(scripts[i].textContent)) {
-      var targetURL = scripts[i].textContent.match(regx).toString()
-                                .replace(/TargetUrl.*\'/, "");
-      location.replace(targetURL);
-      return;
-    }
-  }
-})()
+  var targetURL = script.textContent.match(/TargetUrl.*\'http.*(?=')/).toString()
+                        .replace(/TargetUrl.*\'/, "");
+  location.replace(targetURL);
+}
