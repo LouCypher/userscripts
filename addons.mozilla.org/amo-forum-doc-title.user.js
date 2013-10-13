98bea4d0182870966ae72813beb95a101bf86293
LouCypher <zoolcar9+dev@gmail.com>
2013-10-13 16:04:06 +0700

    Updated various titles

--- addons.mozilla.org/amo-forum-doc-title.user.js
+++ addons.mozilla.org/amo-forum-doc-title.user.js
@@ -1,11 +1,20 @@
 // ==UserScript==
-// @name           AMO Forum Document Title
-// @namespace      loucypher@mozillaca.com
-// @include        https://forums.mozilla.org/addons/*.php*
-// @exclude        https://forums.mozilla.org/addons/index.php*
+// @name          AMO Forum Document Title
+// @namespace     https://userscripts.org/users/12
+// @include       https://forums.mozilla.org/addons/*.php*
+// @exclude       https://forums.mozilla.org/addons/index.php*
+// @grant         none
 // ==/UserScript==
 
-var title = document.title.replace(/\s\u2022.*\-\s/, "");
-title = title.replace("Mozilla Add-ons Forum", "");
+var title = document.title.replace(/^Mozilla Add-ons Forum\s(\-|\u2022)\s/, "");
+
+if (/^View (forum|topic)/.test(title))
+  title = title.replace(/^View (forum|topic)\s\-\s/, "");
+
+if (/^Viewing profile/.test(title))
+  title = title.replace(/^Viewing profile\s\-\s/, "");
+
+if (/^User Control Panel/.test(title))
+  title = title.replace(/^User Control Panel\s\u2022\s/, "") + " \u2022 User Control Panel";
 
 document.title = title + " \u2022 Mozilla Add-ons Forum";
