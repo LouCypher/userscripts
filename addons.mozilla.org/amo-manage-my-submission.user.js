a7496f6f07a54df781b4bd99b49a0c0f5d3b1a13
LouCypher <zoolcar9+dev@gmail.com>
2013-10-28 23:45:36 +0700

    Added compatibility report

--- addons.mozilla.org/amo-manage-my-submission.user.js
+++ addons.mozilla.org/amo-manage-my-submission.user.js
@@ -7,12 +7,12 @@
 // @name            AMO: Manage My Submisions - View Listing Links
 // @namespace       https://userscripts.org/users/12
 // @description     Add/change add-ons view/edit link on AMO DevHub
-// @version         1.0
+// @version         2.0
 // @author          LouCypher
 // @license         MPL 2.0
 // @icon            https://addons.cdn.mozilla.net/media//img/addon-icons/default-32.png
 // @icon64URL       https://addons.cdn.mozilla.net/media//img/addon-icons/default-64.png
-// @screenshot      https://lh4.googleusercontent.com/-VjOzeg6rw5c/Ulpo0aEAxlI/AAAAAAAAD2Y/Ee1wJLyVXsE/s0/amo-manage-my-submission.png
+// @screenshot      https://lh5.googleusercontent.com/-hAU-7oNHm38/Um6TLAwS39I/AAAAAAAAD3k/hWvGCMidfZQ/s200/amo-manage-my-submission.png
 // @contributionURL http://loucypher.github.io/userscripts/donate.html?AMO%3A+Manage+My+Submisions
 // @homepageURL     https://github.com/LouCypher/userscripts/tree/master/addons.mozilla.org
 // @supportURL      https://github.com/LouCypher/userscripts/issues
@@ -23,10 +23,13 @@
 // @grant           GM_addStyle
 // ==/UserScript==
 
-GM_addStyle('.icon-edit {\
+GM_addStyle('.icon-edit, .icon-acr {\
   display: inline-block;\
+  margin-left: .5em;\
   width: 12px;\
   height: 12px;\
+}\
+  .icon-edit {\
   background: url("data:image/png;base64,\
 iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMCAYAAABWdVznAAAAAXNSR0IArs4c6QAAAAZiS0dEAP8A\
 /wD/oL2nkwAAAc1JREFUKM91Ut1LU3EYfn6/386mTYfz40KKJIK2bBWSKNT6AocXtcpcH2ygFxJ1\
@@ -37,8 +40,22 @@ NEpqvkDj71LYTsuAkmfnPMDF3PS1vwTmn/c5XY6JQMdBSStsm2Nvl4WWkQnyOkO1pKOghmiqN8wB\
 oOnB5K16t32y63KjpClbFJtNC20lbyK3xlAlDFZp+GmqNwwAHEPM1SwWXp5udXNzp0jRuSyUlTxh\
 PSvgEDqs+s3dF3de/5HNF7UL3oFOlVekF43nb9bYTlYGMqsM1dBhKXZTOBjbGwrfkmqCHu9ZDLeH\
 2VEjYehpjYmqEnGbepUiPbF/UxRXfG3DR+oU6D9H+Rn7Lx7PpshmY3dzow+j5TqyvJrPGJupOa3S\
-lKeLhfGxDy7M4gkl9nuX3yY23IFrR7ElAAAAAElFTkSuQmCC") 0 0 no-repeat;\
-  margin-left: .5em\
+lKeLhfGxDy7M4gkl9nuX3yY23IFrR7ElAAAAAElFTkSuQmCC") 0 0 no-repeat\
+}\
+  .icon-acr {\
+  background: url("data:image/png;base64,\
+iVBORw0KGgoAAAANSUhEUgAAAAwAAAAMCAYAAABWdVznAAAAAXNSR0IArs4c6QAAAAZiS0dEAP8A\
+/wD/oL2nkwAAAh9JREFUKM890M9Lk3EcB/D353me7ftsJVIuZ8Opaa4fIKQFUjdhQdeQQnbwEHQx\
+uuwSFKzcoT8gBgntYIwKO0iL6BB40SgGIklZplsNtsdn+WM4s/nsefZ8Px2C/oDX5UWJREKdnJx0\
+Y7HY6Wg0+k0IAWZmACAiklIil8udSaVSq8lkUgUzY2JiYuh1NuvajsOGWZEbhsGGYbD5a1OWywZn\
+Mhk3Ho8PMTM0AGhtaRnvPdmnFEubrlnaUDXhsqKqcCwi3yGf2xE6rh5e/T4OYEkBUTt0ffvPgcT6\
+01vK3ssxaKTDrrvYmxnDZvae0rAJrIttELUrH9GbNs71Fdhy4Dv4QV6rgO5gB6l2k3Q7D8/vNWLL\
+gTnYX/iAE2nNhXp5OP32lPboEvRrM6xvlcnTHWQ/M7TROXJag9zkGp1/mHkgoXYpBK74agcRXadG\
+j72Cgu2i/i6NwNob5K0meuwVCEENfbceIXAFzIzU6I3F1fUvnIy2s7etk+8Mgu9eIPa2dfL9kTae\
+fz/Hj6/fXPy/lB/unRowqk8+ha7y7O0rREe72DRNvPJLmnqR5YHKLv282D8F4B/Y/ZofmTW2EAkd\
+wdLyZ0i5DCLCBhHOho9hYWEe+/v7IwDSND09/SwcDseKxaLTbDY9UkoEg0Hs7OzAdV0QEbxerxMI\
+BDylUum5VqvVYkIIR9M0jxCCmZkURWG/3w8pJRERSyk91WrVsSwr9heceQ7HMuS8nAAAAABJRU5E\
+rkJggg==") 0 0 no-repeat\
 }');
 
 var items = $$("#sorter + .items > .item.addon > .info");
@@ -48,12 +65,20 @@ if (items.length) {
     editLink = $("h3 a", items[i]);
     if (editLink) {
       tooltip = $("a.tooltip", items[i]);
-      editLink.parentNode.appendChild(addLink(editLink.getAttribute("href"), tooltip.title));
+      editLink.parentNode.appendChild(addLink(editLink.getAttribute("href"),
+                                              tooltip.title,
+                                              "icon-edit"));
       viewLink = $(".more-actions-popup > ul:last-child > li a");
       if (viewLink) {
         editLink.href = viewLink.getAttribute("href");
         editLink.title = viewLink.textContent;
         editLink.classList.add("tooltip");
+
+        var addonId = items[i].parentNode.dataset.addonid;
+        var acrLink = editLink.parentNode.appendChild(addLink("../compatibility/reporter?guid=" +
+                                                              addonId,
+                                                              "Compatibility reports",
+                                                              "icon-acr"));
       }
       else
         editLink.href = "/" + document.documentElement.lang + "/" + document.body.dataset.app
@@ -71,10 +96,10 @@ function $$(aSelector, aNode) {
   return (aNode || document).querySelectorAll(aSelector);
 }
 
-function addLink(aHref, aTitle) {
+function addLink(aHref, aTitle, aClassName) {
   var link = document.createElement("a");
   link.href = aHref;
-  link.className = "icon-edit tooltip";
+  link.className = aClassName + " tooltip";
   link.title = aTitle;
   return link;
 }
