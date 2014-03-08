bb029dac21f04a4b6b25aa3c9af86789bca72ffc
LouCypher <zoolcar9+dev@gmail.com>
2014-03-09 02:43:06 +0700

    Updated scripts-links/userscript.user.js * Don't install scripts that are under review * Fixed: 'update' links appear on scripts not owned by logged in user

--- greasyfork/scripts-links/userscript.user.js
+++ greasyfork/scripts-links/userscript.user.js
@@ -3,7 +3,7 @@
 // @name            Greasy Fork - Scripts Links
 // @namespace       https://userscripts.org/users/12
 // @description     Add more scripts' links on scripts listing.
-// @version         2.0
+// @version         3.0
 // @author          LouCypher
 // @license         MIT License
 // @contributionURL http://loucypher.github.io/userscripts/donate.html?Greasy+Fork+-+Scripts+Links
@@ -48,6 +48,27 @@ function separate(aNode, aSpaces) {
   return aNode.appendChild(document.createTextNode(aSpaces ? " | " : "|"));
 }
 
+function underReview(aEvent) {
+  aEvent.preventDefault();
+  var scriptTitle = aEvent.target.dataset.scriptTitle;
+  alert(scriptTitle + " is currently under review");
+}
+
+// Opera UserJS doesn't recognize GM API so we rewrite the functions
+if (typeof GM_addStyle !== "function" && GM_log !== "function") {
+  function GM_addStyle(aCSS) {
+    var style = document.createElement("style");
+    style.type = "text/css";
+    style.textContent = aCSS;
+    $("head").appendChild(style);
+  }
+
+  function GM_log(aText) {
+    console.log(aText);
+  }
+}
+
+var blocked = /(94|115|116|117|119|120|121|122|123|172)$/;
 var user = $(".user-profile-link a"); // Check if you're logged in
 
 /* Get CSRF authenticity token */
@@ -61,15 +82,22 @@ var scriptBoxes = $$(".script-list article");
 if (scriptBoxes.length) {
   GM_addStyle(".script-list li{height:200px!important}");
 
-  var box, node, href;
+  var box, node, name, href, install;
   for (var i = 0; i < scriptBoxes.length; i++) {
     box = scriptBoxes[i];
-    node = box.insertBefore(createElement("p"), $("footer", box));
+    name = $("h2 a", box).textContent;  // Script title
     href = $("h2 a", box).getAttribute("href"); // Script page URL
+    node = box.insertBefore(createElement("p"), $("footer", box));
 
-    node.appendChild(addLink(href + "/code.user.js", "install", "",
-                             href + "/install-ping"
-                                  + (authToken ? authToken : "")));
+    install = node.appendChild(addLink(href + "/code.user.js", "install", "",
+                                       href + "/install-ping"
+                                            + (authToken ? authToken : "")));
+    if (blocked.test(href)) {
+      install.href = "/scripts/under-assessment";
+      install.removeAttribute("data-ping-url");
+      install.dataset.scriptTitle = name;
+      install.addEventListener("click", underReview);
+    }
 
     separate(node, true);
     node.appendChild(addLink(href + "/code", "code"));
@@ -99,18 +127,25 @@ document.addEventListener("DOMContentLoaded", function() {
     var cells = $$("td:nth-child(2)", scriptTable);
     //GM_log(cells.length);
     if (cells.length) {
-      var cell, node, link, href, authorColumn, mine;
+      var cell, node, link, name, href, install, userId, authorColumn, mine;
       for (var i = 0; i < cells.length; i++) {
         cell = cells[i];
+        name = $("a", cell).textContent;  // Script title
         link = $("a", cell);  // Script page link
         href = link.getAttribute("href"); // Script page URL
 
         node = cell.appendChild(createElement("span"));
 
-        node.appendChild(addLink(href + "/code.user.js", "I",
-                                 "Install " + link.textContent,
-                                 href + "/install-ping"
-                                      + (authToken ? authToken : "")));
+        install = node.appendChild(addLink(href + "/code.user.js", "I",
+                                           "Install " + link.textContent,
+                                           href + "/install-ping"
+                                                + (authToken ? authToken : "")));
+        if (blocked.test(href)) {
+          install.href = "/scripts/under-assessment";
+          install.removeAttribute("data-ping-url");
+          install.dataset.scriptTitle = name;
+          install.addEventListener("click", underReview);
+        }
 
         separate(node);
         node.appendChild(addLink(href + "/code", "C", "Code"));
@@ -121,18 +156,22 @@ document.addEventListener("DOMContentLoaded", function() {
         separate(node);
         node.appendChild(addLink(href + "/feedback", "F", "Feedback"));
 
-        // Check if you're logged in and it's own your script
-        authorColumn = $("td:nth-child(4) a", cell.parentNode);
-        if (authorColumn)
-          mine = authorColumn.href === user.href;
-        else
-          mine = location.href.indexOf(user.href) === 0;
-        
-        // Add 'update' link if you're logged in and it's your own script
-        if (user && mine) {
-          separate(node);
-          node.appendChild(addLink(href + "/versions/new", "U",
-                                   "Update " + link.textContent));
+        if (user) { // If logged in
+          userId = parseInt(user.href.match(/\d+/));
+
+          // Check if it's own your script
+          authorColumn = $("td:nth-child(4) a", cell.parentNode);
+          if (authorColumn)
+            mine = authorColumn.href === user.href;
+          else
+            mine = parseInt(location.href.match(/\d+/)) === userId;
+
+          // Add 'update' link if you're logged in and it's your own script
+          if (mine) {
+            separate(node);
+            node.appendChild(addLink(href + "/versions/new", "U",
+                                     "Update " + link.textContent));
+          }
         }
       }
     }
