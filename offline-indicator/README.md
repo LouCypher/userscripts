04f2ee1abc3d20156a1ed4fc6b57c76e44175ec2
LouCypher <zoolcar9+dev@gmail.com>
2014-03-14 14:32:15 +0700

    Updated /offline-indicator/ user script * New feature: change icon position by clicking on it. * Upgrade license to MIT License.

--- offline-indicator/README.md
+++ offline-indicator/README.md
@@ -16,11 +16,11 @@ Scriptish, Greasemonkey, Tampermonkey, Violentmonkey.
 
 [![Screenshot](https://lh3.googleusercontent.com/-m2Zcc5U8dV8/UyIhH-gpb_I/AAAAAAAAEDU/fGWgywPc1us/s400/screenshot.png)](https://lh3.googleusercontent.com/-m2Zcc5U8dV8/UyIhH-gpb_I/AAAAAAAAEDU/fGWgywPc1us/s0/screenshot.png "Click to see the full-size image")
 
-### Licenses
+### License
 
-* User script: [WTFPL](http://www.wtfpl.net/txt/copying/).
+[MIT License](https://raw.github.com/LouCypher/userscripts/master/licenses/MIT/LICENSE.txt).
 
-* CSS: [MIT License](https://raw.github.com/LouCypher/userscripts/master/licenses/MIT/LICENSE.txt).
+### Credits
 
-* Icon by [Tango! Desktop Project](http://tango.freedesktop.org/Tango_Desktop_Project).
+Icon by [Tango! Desktop Project](http://tango.freedesktop.org/Tango_Desktop_Project).  
 It has been released into the [public domain](http://tango.freedesktop.org/Frequently_Asked_Questions#Terms_of_Use.3F) by its author.
--- offline-indicator/userscript.user.js
+++ offline-indicator/userscript.user.js
@@ -1,19 +1,37 @@
-/* This program is free software. It comes without any warranty, to
- * the extent permitted by applicable law. You can redistribute it
- * and/or modify it under the terms of the Do What The Fuck You Want
- * To Public License, Version 2, as published by Sam Hocevar. See
- * http://www.wtfpl.net/ for more details. */
+/**
+ * Offline Indicator user script
+ * Licensed under the MIT license
+ *
+ * Copyright (c) Zulkarnain K.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ **/
 
 // ==UserScript==
 // @id              offline-indicator@loucypher
 // @name            Offline Indicator
 // @namespace       https://github.com/LouCypher/userscripts
 // @description     Add icon to web page on bottom right as indicator when in offline mode.
-// @version         2.1
+// @version         3.0
 // @author          LouCypher
 // @contributor     Tango! Desktop Project (icon)
-// @license         WTFPL
-// @CSS-license     MIT License
+// @license         MIT License
 // @SVG-license     Public domain
 // @icon            https://raw.github.com/LouCypher/userscripts/master/offline-indicator/icon48.png
 // @icon64URL       https://raw.github.com/LouCypher/userscripts/master/offline-indicator/icon64.png
@@ -25,15 +43,21 @@
 // @resource        CSS https://raw.github.com/LouCypher/userscripts/master/offline-indicator/offline-indicator.css
 // @resource        SVG https://raw.github.com/LouCypher/userscripts/master/offline-indicator/offline-indicator.svg
 // @resource        CHANGELOG https://raw.github.com/LouCypher/userscripts/master/offline-indicator/CHANGELOG.txt
-// @resource        LICENSE https://raw.github.com/LouCypher/userscripts/master/licenses/WTFPL/LICENSE.txt
+// @resource        LICENSE https://raw.github.com/LouCypher/userscripts/master/licenses/MIT/LICENSE.txt
 // @include         /^(https?|ftp|unmht):.*/
 // @grant           GM_addStyle
 // @grant           GM_getResourceURL
 // @grant           GM_getResourceText
+// @grant           GM_getValue
+// @grant           GM_setValue
 // ==/UserScript==
 
 const DIV_ID = "offline-indicator";
 const DIV_CLASS = "browser-is-offline";
+const ICON_TOP = "offline-indicator-icon-top";
+const ICON_RIGHT = "offline-indicator-icon-right";
+const ICON_BOTTOM = "offline-indicator-icon-bottom";
+const ICON_LEFT = "offline-indicator-icon-left";
 
 function $(aId) {
   return document.getElementById(aId);
@@ -41,6 +65,42 @@ function $(aId) {
 
 function toggleIndicator() {
   $(DIV_ID).classList.toggle(DIV_CLASS);
+}
+
+function changePosition() {
+  var div = $(DIV_ID);
+  var isTop = div.classList.contains(ICON_TOP);
+  var isRight = div.classList.contains(ICON_RIGHT);
+  var isBottom = div.classList.contains(ICON_BOTTOM);
+  var isLeft = div.classList.contains(ICON_LEFT);
+
+  // If bottom-right, move to bottom-left
+  if (isBottom && isRight) {
+    div.classList.add(ICON_LEFT);
+    div.classList.remove(ICON_RIGHT);
+    GM_setValue("position", 1);
+  }
+
+  // If bottom-left, move to top-left
+  else if (isBottom && isLeft) {
+    div.classList.add(ICON_TOP);
+    div.classList.remove(ICON_BOTTOM);
+    GM_setValue("position", 2);
+  }
+
+  // If top-left, move to top-right
+  else if (isTop && isLeft) {
+    div.classList.add(ICON_RIGHT);
+    div.classList.remove(ICON_LEFT);
+    GM_setValue("position", 3);
+  }
+
+  // If top-right, move to bottom-right
+  else if (isTop && isRight) {
+    div.classList.add(ICON_BOTTOM);
+    div.classList.remove(ICON_TOP);
+    GM_setValue("position", 0);
+  }
 }
 
 // Run on HTML document only
@@ -51,11 +111,31 @@ if (document instanceof HTMLDocument) {
   var div = document.documentElement.appendChild(document.createElement("div"));
   div.id = DIV_ID;
 
+  var position = GM_getValue("position", 0);
+  switch (position) {
+    case 3:   // top-right
+      div.classList.add(ICON_TOP);
+      div.classList.add(ICON_RIGHT);
+      break;
+    case 2:   // top-left
+      div.classList.add(ICON_TOP);
+      div.classList.add(ICON_LEFT);
+      break;
+    case 1:   // bottom-left
+      div.classList.add(ICON_BOTTOM);
+      div.classList.add(ICON_LEFT);
+      break;
+    default:  // bottom-right
+      div.classList.add(ICON_BOTTOM);
+      div.classList.add(ICON_RIGHT);
+  }
+
   var img = div.appendChild(document.createElement("img"));
   img.src = GM_getResourceURL("SVG");
   if (typeof opera === "object")
     img.src = img.src.replace(/^data:/, "data:image/svg+xml");
   img.alt = img.title = "Working offline";
+  img.addEventListener("click", changePosition);
 
   if (!navigator.onLine)
     toggleIndicator();
