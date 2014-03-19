2927d669fa0408b528130765bed0e23092e74e28
LouCypher <zoolcar9+dev@gmail.com>
2014-03-19 23:32:31 +0700

    Added user discussions link

--- greasyfork/script-counter/userscript.user.js
+++ greasyfork/script-counter/userscript.user.js
@@ -9,7 +9,7 @@
 // @name            Greasy Fork - Script Counter
 // @namespace       https://github.com/LouCypher/userscripts
 // @description     Add number of scripts on user's profile page
-// @version         2.1
+// @version         3.0
 // @author          LouCypher
 // @license         WTFPL
 // @screenshot      https://raw.github.com/LouCypher/userscripts/master/greasyfork/script-counter/screenshot.png
@@ -25,8 +25,32 @@
 // @grant           none
 // ==/UserScript==
 
+function $(aSelector, aNode) {
+  return (aNode || document).querySelector(aSelector);
+}
+
+function $$(aSelector, aNode) {
+  return (aNode || document).querySelectorAll(aSelector);
+}
+
+function createElement(aTagName) {
+  return document.createElement(aTagName);
+}
+
+function createText(aText) {
+  return document.createTextNode(aText);
+}
+
+function createLink(aURL, aText, aName) {
+  var link = createElement("a");
+  aURL && (link.href = aURL);
+  aText && (link.textContent = aText);
+  aName && (link.name = aName);
+  return link;
+}
+
 function showError() {
-  var pAlert = document.querySelector("p.alert");
+  var pAlert = $("p.alert");
   if (pAlert) {  // Display error notification
     pAlert.classList.add("important");
     pAlert.innerHTML = '<a href="/scripts/180">Script Counter</a>'
@@ -35,22 +59,40 @@ function showError() {
                      + 'Discussion/ScriptID=180">notify</a>'
                      + ' the author.';
   }
-  else  // Show error in console
-    throw new Error("Some thing went wrong.");
+  else  // Log in console
+    console.log("Some thing went wrong.");
 }
 
 var scripts;
 
-var scriptList = document.querySelectorAll("#user-script-list > li");
-if (scriptList.length)
-  scripts = scriptList.length;  // User has script(s)
+var scriptEntries = $$("#user-script-list > li");
+if (scriptEntries.length)
+  scripts = scriptEntries.length;  // User has script(s)
 
 if (scripts) {  // If user has script(s)
-  var username = document.querySelector("h2");
-  var title = document.querySelector("body > section:not([id]) h3");
-  if (title)  // add number of script(s) in Scripts section
-    title.appendChild(document.createTextNode(" (" + scripts + ")"));
+  var userID = location.pathname.match(/\d+/).toString();
+  var userName = $("h2").textContent;
+  var userForumURL = "/forum/?Discussion/ScriptAuthorID=" + userID;
+
+  // Add number of script(s) in Scripts section
+  var title = $("body > section:not([id]) h3");
+  if (title)
+    title.appendChild(createText(" (" + scripts + ")"));
   else
     showError(); // Sam ting wen wong
-}
-// else user didn't post any scripts
+
+  // Add "discussions on user's scripts" link
+  var userForum = $("#user-discussions-on-scripts-written ul");
+  if (userForum) {
+    userForum.appendChild(createElement("li"))
+             .appendChild(createLink(userForumURL, "More discussions\u2026"));
+  }
+  else {
+    var scriptList = $("#user-script-list") || $("#table-container");
+    if (scriptList) {
+      $("header", scriptList.parentNode).appendChild(createElement("p")).
+      appendChild(createLink(userForumURL, "Discussions on " + userName +
+                                           "'s scripts"));
+    }
+  }
+} // else user doesn't have any scripts or the scripts are unlisted/deleted
